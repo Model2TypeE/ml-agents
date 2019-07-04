e395203223a3cc129d95d471c88ac7485923a0b8
Jan <33833587+Model2TypeE@users.noreply.github.com>
2019-07-05 01:58:25 +0200

    working hinge joint with debug controller

--- UnitySDK/Assets/ML-Agents/Polymorphic Project/Code/JointTester.cs
+++ UnitySDK/Assets/ML-Agents/Polymorphic Project/Code/JointTester.cs
@@ -2,15 +2,33 @@
 
 public class JointTester : MonoBehaviour
 {
-	[SerializeField] HingeJoint joint;
-
-	private void Start()
+	private float targetVelocity = 0f;
+	public float TargetVelocity
 	{
-		
+		get { return targetVelocity; }
+		set
+		{
+			targetVelocity = value;
+			motor.targetVelocity = value;
+			joint.motor = motor;
+		}
+	}
+
+	[SerializeField] HingeJoint joint;
+	[SerializeField] float velocityChange;
+
+	private JointMotor motor;
+
+	private void Awake()
+	{
+		motor = joint.motor;
 	}
 
 	private void Update()
 	{
+		if (Input.GetKey(KeyCode.A)) TargetVelocity = -velocityChange;
 		
+		else if (Input.GetKey(KeyCode.D)) TargetVelocity = velocityChange;
+		else TargetVelocity = 0f;
 	}
 }
